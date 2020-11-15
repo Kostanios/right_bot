5a2f028b6d0611a649162a9f8e0582af169dfb3d
Kostanios <kotov.const@mail.ru>
2020-11-15 22:18:57 +0300

    feat: simple commands

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const PORT = process.env.PORT || 3000;
 
-const token = require('./static/token.js');
+const { prefix, token } = require('./config.json');
 
 const app = express();
 
@@ -13,9 +13,60 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === '!ping') {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    if (message.content === `${prefix}ping`) {
         // send back "Pong." to the channel the message was sent in
         message.channel.send('Pong.');
+    }
+    else if (message.content === `${prefix}beep`) {
+        message.channel.send('Boop.');
+    }
+    else if (message.content === `${prefix}server`) {
+        message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+    }
+    else if (message.content === `${prefix}user-info`) {
+        message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+    }
+    else if (command === 'args-info') {
+        if (!args.length) {
+            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+        }
+        else if (args[0] === 'foo') {
+            return message.channel.send('bar');
+        }
+        message.channel.send(`First argument: ${args[0]}`);
+    }
+    else if (command === 'kick') {
+        // grab the "first" mentioned user from the message
+        // this will return a `User` object, just like `message.author`
+        if(!message.mentions.users.size) {
+            return message.reply('who should I kick?');
+        }
+        const taggedUser = message.mentions.users.first();
+        message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+    }
+    else if (command === 'avatar') {
+        if (!message.mentions.users.size) {
+            return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: 'png', dynamic: true })}>`);
+        }
+        const avatarList = message.mentions.users.map(user => {
+            return `${user.username}'s avatar: <${user.displayAvatarURL({ format: 'png', dynamic: true })}>`;
+        });
+        // send the entire array of strings as a message
+        // by default, discord.js will `.join()` the array with `\n`
+        message.channel.send(avatarList);
+    }
+    else if (command === 'prune') {
+        const amount = parseInt(args[0]) + 1;
+        if (isNaN(amount)) {
+            return message.reply('that doesn\'t seem to be a valid number.');
+        }
+        else if (amount <= 1 || amount > 100) {
+            return message.reply('you need to input a number between 1 and 99.');
+        }
+        message.channel.bulkDelete(amount);
     }
 	console.log(message.content);
 });
